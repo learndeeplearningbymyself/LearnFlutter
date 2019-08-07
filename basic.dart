b0d4b330d0f5b28a9acdbbbafa62887103421f49
tt-anh-eole <tt-anh@eole.co.jp>
2019-08-07 18:32:30 +0900

    Add basic dart code

--- basic.dart
+++ basic.dart
@@ -2,9 +2,31 @@ printInteger(int aNumber) {
   print('The number is $aNumber');
 }
 
-main() {
+// This is comment
+
+class Person {
+  String name;
+  int age;
+  
+  Person({String name, int age}) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Person1 {
+  String name;
+  int age;
+  
+  Person1({this.name, this.age = 30});
+}
+
+void main() {
+  var p1 = Person(name: 'Max',age: 30);
+  var p2 = Person(age: 31, name: 'Manu');
+
+  var p3 = Person1(name: 'Haha');
+
   var number = 42;
   printInteger(number);
 }
-
-// This is comment
